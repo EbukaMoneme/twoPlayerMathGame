b0225f90cc6dc9a94dd0d8efd581098129fdc50e
Ebuka Moneme <ebukamoneme@Ebukas-MacBook-Pro.local>
2021-11-08 14:36:35 -0500

    Create classes, initialize game object in main.

---
+++ game.rb
@@ -0,0 +1,18 @@
+class Game
+	def initialize()
+		puts "Game started!"
+	end
+
+
+	def new_turn()
+
+	end
+
+	def response()
+
+	end
+
+	def check_answer()
+
+	end
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'game.rb'
+require_relative 'question.rb'
+require_relative 'player.rb'
+
+new_game = Game.new()
